a7ebd6edd14257205c6cb15a758e78856a6ceccf
Pacrette <pacrette@protonmail.com>
2022-04-08 02:11:58 +0200

    fishing net softbody

---
+++ scenes/net/fishing_net.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+
+export var attach1: NodePath
+export var attach2: NodePath
+export var attach3: NodePath
+export var attach4: NodePath
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
