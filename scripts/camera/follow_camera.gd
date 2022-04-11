69f569ba2444af2b0d098fba2d1933369f60c61c
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-04-11 21:46:20 +0200

    Improved follow camera

--- scripts/camera/follow_camera.gd
+++ scripts/camera/follow_camera.gd
@@ -12,6 +12,10 @@ func _physics_process(delta: float) -> void:
 	if not target:
 		return
 	
+	var current_distance: float = global_transform.origin.distance_to(target.global_transform.origin)
 	var direction: Vector3 = global_transform.origin.direction_to(target.global_transform.origin)
-	global_transform = global_transform.interpolate_with(target.global_transform, speed * delta) # .origin.linear_interpolate(target.global_transform.origin + direction * distance, delta)
+	
 	look_at(target.global_transform.origin, Vector3.UP)
+	
+	if current_distance > distance:
+		global_transform.origin = global_transform.origin.linear_interpolate(target.global_transform.origin + direction * distance, delta)
