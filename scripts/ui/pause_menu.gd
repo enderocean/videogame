6e9b66a228fcd6d6b2844e28d3b1a35171b2d490
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-04-14 01:02:41 +0200

    Improved popups and pausing game method

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -1,13 +1,9 @@
-extends ColorRect
+extends Popup
+class_name PauseMenu
 
 export var settings_panel_path: NodePath
 onready var settings_panel: SettingsPanel = get_node(settings_panel_path)
 
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		visible = !visible
-		settings_panel.visible = false
 
 func _on_ReturnToGame_pressed() -> void:
 	visible = false
@@ -29,9 +25,9 @@ func _on_BackToMap_pressed() -> void:
 	SceneLoader.load_scene("res://scenes/ui/missions.tscn")
 
 
-func _on_visibility_changed() -> void:
-	get_tree().paused = visible
-
-
 func _on_Ping360Toggle_toggled(button_pressed: bool) -> void:
 	Globals.ping360_enabled = button_pressed
+
+
+func _on_visibility_changed() -> void:
+	settings_panel.visible = false
