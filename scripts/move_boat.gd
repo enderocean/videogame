60debb9b3f6d0c593ec1070f405c68e913af4924
denis.bled@newmips.com <denis.bled@newmips.com>
2022-04-16 04:17:39 +0200

    Ender station

--- scripts/move_boat.gd
+++ scripts/move_boat.gd
@@ -4,14 +4,13 @@ extends KinematicBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var point = Vector3(0,0,120)
-
-
+var point = Vector3(0,-5,120)
 
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	var direction = (point - transform.origin)/100
+	yield(get_tree().create_timer(5.0), "timeout")
+	var direction = (point - transform.origin)/200
 	move_and_slide(direction)
 
 
