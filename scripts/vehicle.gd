fc2aa927d0d7f88ed5bbdae104712969943be251
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-04-19 16:49:57 +0200

    Improved rope creation

--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -1,8 +1,11 @@
 extends Spatial
 class_name Vehicle
 
+export var vehicle_path: NodePath
+onready var vehicle: PhysicsBody = get_node_or_null(vehicle_path)
+
 export var camera_target_path: NodePath
 onready var camera_target: Spatial = get_node_or_null(camera_target_path)
 
 export var theter_anchor_path: NodePath
-onready var theter_anchor: PhysicsBody = get_node_or_null(theter_anchor_path)
+onready var theter_anchor: Spatial = get_node_or_null(theter_anchor_path)
