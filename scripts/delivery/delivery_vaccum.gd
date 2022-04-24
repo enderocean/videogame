7f3dc5acf47a7a2b2c841fc0f370c790aa7dbac3
Pacrette <pacrette@protonmail.com>
2022-04-24 20:27:24 +0200

    vaccum tool finish

---
+++ scripts/delivery/delivery_vaccum.gd
@@ -0,0 +1,62 @@
+extends "res://scripts/delivery/delivery_area.gd"
+class_name DeliveryVaccum
+
+var removed_bodys = []
+
+onready var end_pipe = get_node("Position3D")
+
+func _physics_process(delta):
+	for id in objects.size():
+		if (removed_bodys.find(objects[id]) == -1):
+			var object = instance_from_id(objects[id])
+			object.global_transform.origin = object.global_transform.origin.linear_interpolate(end_pipe.global_transform.origin, 0.1)
+			if (object.global_transform.origin.distance_squared_to(end_pipe.global_transform.origin) < 0.01):
+				removed_bodys.append(objects[id])
+				object.queue_free()
+				emit_signal("objects_changed", self, objects)
+
+func _on_body_entered(body: Node) -> void:
+	# Make sure the body is a Deliverable
+
+	if not body is DeliveryObject:
+		return
+
+	var object: DeliveryObject = body
+
+	# Check if the object has the same objective_type
+	if not object.is_in_group(group):
+		return
+
+	# Make sure the object is not already in the area
+	var id: int = object.get_instance_id()
+	if objects.has(id):
+		return
+	
+	# Play random sound from array
+	RadioSounds.play(RADIO_SOUNDS[RadioSounds.rand.randi_range(0, RADIO_SOUNDS.size() - 1)])
+
+	# Make the object delivered
+	object.delivered = true
+	objects.append(id)
+	#emit_signal("objects_changed", self, objects)
+
+
+func _on_body_exited(body: Node) -> void:
+	if only_enter:
+		return
+	
+	# Make sure the body is a Deliverable
+	if not body is DeliveryObject:
+		return
+
+	# Make sure the object is in the area
+	var object: DeliveryObject = body
+	var id: int = object.get_instance_id()
+	var index: int = objects.find(id)
+	if index == -1:
+		return
+
+	# Make the object not delivered
+	object.delivered = false
+	objects.remove(index)
+	#emit_signal("objects_changed", self, objects)
--- scripts/vehicle/vacuum_tool.gd
+++ scripts/vehicle/vacuum_tool.gd
@@ -1,18 +1,2 @@
 extends "res://scripts/vehicle/vehicle_tool.gd"
 class_name VacuumTool
-
-var area_bodys = []
-var removed_bodys = []
-
-func _ready() -> void:
-	
-	get_node("VacuumRigidBody/VaccumProcess").connect("objects_changed", self, "_on_Vaccum_objects_changed")
-
-func _on_Vaccum_objects_changed(objects: Array) -> void:
-	for id in objects.size():
-		if (removed_bodys.find(objects[id]) == -1):
-			removed_bodys.append(objects[id])
-			instance_from_id(objects[id]).queue_free()
-
-	print(removed_bodys.size())
-		
