3ea80786a432f0f0c996a6c5f07a5bdd7aa84ee8
Pacrette <pacrette@protonmail.com>
2022-05-03 19:27:41 +0200

    update cut pos

--- scripts/vehicle/cutter_tool.gd
+++ scripts/vehicle/cutter_tool.gd
@@ -6,13 +6,9 @@ func _on_RightCutArea_area_shape_entered(area_rid, area, area_shape_index, local
 		var parent = area.get_parent()
 
 		var node_point = get_node("CutRightRigidBody/RightCutArea")
-		var p = parent.global_transform.xform(node_point.global_transform.origin)
-		var d1 = parent.pospins[area.rope_id - 1].global_transform.origin.distance_squared_to(p)
-		var d2 = parent.attachs[area.rope_id - 1].global_transform.origin.distance_squared_to(p)
-		
-		print("p = ", p)
-		print("d1 = ", d1)
-		print("d2 = ", d2)
-		print("d1/d2 = ", d1/d2)
-		print("(d1/d2) / 2  = ", (d1/d2) / 2)
-		parent.cut_rope(area, (area.rope_id - 1), (d1/d2) / 2)
+		var d1 = parent.pospins[area.rope_id - 1].global_transform.origin.distance_squared_to(node_point.global_transform.origin)
+		var d2 = parent.attachs[area.rope_id - 1].global_transform.origin.distance_squared_to(node_point.global_transform.origin)
+	
+		var cut_pos = d2 / (d1 + d2)
+		parent.cut_rope(area, (area.rope_id - 1), cut_pos)
+
