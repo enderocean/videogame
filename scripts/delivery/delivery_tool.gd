da445430b24092c5d7f64820f7d7f56b13b3e8e6
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-05-06 17:59:45 +0200

    Started delivery tools

--- scripts/delivery/delivery_tool.gd
+++ scripts/delivery/delivery_tool.gd
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 extends Spatial
 class_name DeliveryTool
 
@@ -7,5 +8,11 @@ onready var theter_anchor: TheterAnchor = get_node(theter_anchor_path)
 var objective_type = Globals.ObjectiveType.MAGNET
 var carried: bool = false
 var group: String
+=======
+extends RigidBody
+class_name DeliveryTool
+
+var objective_type = Globals.ObjectiveType.MAGNET
+>>>>>>> 6d9f676 (Started delivery tools)
 
 signal catched
--- scripts/delivery/magnet_delivery_tool.gd
+++ scripts/delivery/magnet_delivery_tool.gd
@@ -1,6 +1,7 @@
 extends "res://scripts/delivery/delivery_tool.gd"
 class_name MagnetDeliveryTool
 
+<<<<<<< HEAD
 export var magnet_body_path: NodePath
 onready var magnet_body: RigidBody = get_node(magnet_body_path)
 
@@ -48,18 +49,36 @@ func _physics_process(delta: float) -> void:
 			joint.set_node_b(detected_object.get_path())
 			
 			emit_signal("catched")
+=======
+export var area: NodePath
+
+var detected_object: DeliveryObject
+
+func _ready() -> void:
+	.ready()
+	objective_type = Globals.ObjectiveType.MAGNET
+	(get_node(area) as Area).connect("area_entered", self, "_on_body_entered")
+>>>>>>> 6d9f676 (Started delivery tools)
 
 
 func _on_body_entered(body: Node) -> void:
 	# Make sure the body is a Deliverable
+<<<<<<< HEAD
 	if not body is DeliveryObject:
 		return
 	
+=======
+
+	if not body is DeliveryObject:
+		return
+
+>>>>>>> 6d9f676 (Started delivery tools)
 	var object: DeliveryObject = body
 
 	# Check if the object has the same objective_type
 	if not object.is_in_group(group):
 		return
+<<<<<<< HEAD
 	
 	# Make sure the object is not already in the area
 	if detected_object and object.get_instance_id() == detected_object.get_instance_id():
@@ -70,10 +89,34 @@ func _on_body_entered(body: Node) -> void:
 
 
 func _on_body_exited(body: Node) -> void:
+=======
+
+	# Make sure the object is not already in the area
+	var id: int = object.get_instance_id()
+	if objects.has(id):
+		return
+
+	# Don't play a radio sound for the vacuum
+	if objective_type != Globals.ObjectiveType.VACUUM:
+		# Play random sound from array
+		RadioSounds.play(RADIO_SOUNDS[RadioSounds.rand.randi_range(0, RADIO_SOUNDS.size() - 1)])
+
+	# Make the object delivered
+	object.delivered = true
+	objects.append(id)
+	emit_signal("objects_changed", self, objects)
+
+
+func _on_body_exited(body: Node) -> void:
+	if only_enter:
+		return
+	
+>>>>>>> 6d9f676 (Started delivery tools)
 	# Make sure the body is a Deliverable
 	if not body is DeliveryObject:
 		return
 
+<<<<<<< HEAD
 	var object: DeliveryObject = body
 
 	# Check if the object has the same objective_type
@@ -86,3 +129,16 @@ func _on_body_exited(body: Node) -> void:
 	
 	raycast.enabled = false
 	detected_object = null
+=======
+	# Make sure the object is in the area
+	var object: DeliveryObject = body
+	var id: int = object.get_instance_id()
+	var index: int = objects.find(id)
+	if index == -1:
+		return
+
+	# Make the object not delivered
+	object.delivered = false
+	objects.remove(index)
+	emit_signal("objects_changed", self, objects)
+>>>>>>> 6d9f676 (Started delivery tools)
