ad7787f3e87de25029f316facf32636ac2ec6dc5
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-05-10 13:29:20 +0200

    Fixed rebase conflict for some reasons

--- scripts/delivery/delivery_tool.gd
+++ scripts/delivery/delivery_tool.gd
@@ -2,9 +2,6 @@ extends RigidBody
 class_name DeliveryTool
 
 var objective_type = Globals.ObjectiveType.MAGNET
-<<<<<<< HEAD
 var group: String
-=======
->>>>>>> 41f82b2 (Started delivery tools)
 
 signal catched
