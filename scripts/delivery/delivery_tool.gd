97dd83b9e0283476dae17c7638ba79d27cf4c4a3
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-05-11 15:12:11 +0200

    Removed conflicts texts

--- scripts/delivery/delivery_tool.gd
+++ scripts/delivery/delivery_tool.gd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extends Spatial
 class_name DeliveryTool
 
@@ -8,11 +7,5 @@ onready var theter_anchor: TheterAnchor = get_node(theter_anchor_path)
 var objective_type = Globals.ObjectiveType.MAGNET
 var carried: bool = false
 var group: String
-=======
-extends RigidBody
-class_name DeliveryTool
-
-var objective_type = Globals.ObjectiveType.MAGNET
->>>>>>> 6d9f676 (Started delivery tools)
 
 signal catched
