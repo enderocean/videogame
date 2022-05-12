cc1892b7e29edbd4c498e9c89e71fe415355ed45
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-05-12 15:50:27 +0200

    Started grappling hook

--- scripts/delivery/delivery_tool.gd
+++ scripts/delivery/delivery_tool.gd
@@ -4,6 +4,9 @@ class_name DeliveryTool
 export var theter_anchor_path: NodePath
 onready var theter_anchor: TheterAnchor = get_node(theter_anchor_path)
 
+export var tool_body_path: NodePath
+onready var tool_body: RigidBody = get_node(tool_body_path)
+
 var objective_type = Globals.ObjectiveType.MAGNET
 var carried: bool = false
 var group: String
--- scripts/delivery/grappling_hook_delivery_tool.gd
+++ scripts/delivery/grappling_hook_delivery_tool.gd
@@ -1,14 +1,9 @@
 extends "res://scripts/delivery/delivery_tool.gd"
-class_name MagnetDeliveryTool
-
-export var stickpoint_path: NodePath
-onready var stickpoint: Position3D = get_node(stickpoint_path)
+class_name GrapplingHookDeliveryTool
 
 export var raycast_path: NodePath
 onready var raycast: RayCast = get_node(raycast_path)
 
-export var sticking_distance: float = 0.01
-export var magnet_force: float = 1.0
 
 var detected_object: DeliveryObject
 var sticked: bool = false
@@ -24,27 +19,27 @@ func _physics_process(delta: float) -> void:
 	if not detected_object or carried:
 		return
 
-	if raycast.is_colliding():
-		var collision_point: Vector3 = raycast.get_collision_point()
-		var collision_normal: Vector3 = raycast.get_collision_normal()
-		
-		var distance: float = collision_point.distance_to(stickpoint.global_transform.origin)
-		if distance <= sticking_distance:
-			raycast.enabled = false
-			detected_object.mass = 1.0
+#	if raycast.is_colliding():
+#		var collision_point: Vector3 = raycast.get_collision_point()
+#		var collision_normal: Vector3 = raycast.get_collision_normal()
+#
+#		var distance: float = collision_point.distance_to(stickpoint.global_transform.origin)
+#		if distance <= sticking_distance:
+#			raycast.enabled = false
+#			detected_object.mass = 1.0
+#
+#			var joint: HingeJoint = HingeJoint.new()
+#			joint.set_param(HingeJoint.PARAM_BIAS, 1.0)
+#			joint.set_flag(HingeJoint.FLAG_USE_LIMIT, true)
+#			joint.set_param(HingeJoint.PARAM_LIMIT_UPPER, 0.0)
+#			joint.set_param(HingeJoint.PARAM_LIMIT_LOWER, 0.0)
+#			add_child(joint)
+#
+#			joint.global_transform.origin = collision_point
+#			joint.set_node_a(tool_body.get_path())
+#			joint.set_node_b(detected_object.get_path())
 			
-			var joint: HingeJoint = HingeJoint.new()
-			joint.set_param(HingeJoint.PARAM_BIAS, 1.0)
-			joint.set_flag(HingeJoint.FLAG_USE_LIMIT, true)
-			joint.set_param(HingeJoint.PARAM_LIMIT_UPPER, 0.0)
-			joint.set_param(HingeJoint.PARAM_LIMIT_LOWER, 0.0)
-			add_child(joint)
-			
-			joint.global_transform.origin = collision_point
-			joint.set_node_a(tool_body.get_path())
-			joint.set_node_b(detected_object.get_path())
-			
-			emit_signal("catched")
+#			emit_signal("catched")
 
 
 func _on_body_entered(body: Node) -> void:
