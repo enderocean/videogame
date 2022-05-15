3d69d75a2c734a70b40ba37d8685baab2be8c688
denis.bled@newmips.com <denis.bled@newmips.com>
2022-05-15 13:11:15 +0200

    Fixing levels

---
+++ scripts/whaleshark.gd
@@ -0,0 +1,18 @@
+extends Path
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var t := 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	t += delta
+	var node = get_node("PathFollow_whale")
+	node.offset = t
