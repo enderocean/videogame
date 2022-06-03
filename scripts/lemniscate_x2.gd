4aa92f3c3f93666df13d3571923afb607f50f818
denis.bled@newmips.com <denis.bled@newmips.com>
2022-06-03 17:42:33 +0200

    Fix levels

---
+++ scripts/lemniscate_x2.gd
@@ -0,0 +1,19 @@
+extends Path
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var t := 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	t += 2*delta
+	var node = get_child(0) # get_node("PathFollow")
+	node.offset = t
+	
