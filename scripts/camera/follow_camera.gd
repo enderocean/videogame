da1f79b56ee1f00332b8c509a39e9db9d4d6c9b8
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-06-27 15:01:03 +0200

    Restored follow camera behaviour

--- scripts/camera/follow_camera.gd
+++ scripts/camera/follow_camera.gd
@@ -1,8 +1,8 @@
 extends Camera
 class_name FollowCamera
 
-export var speed: float = 1
-export var distance: float = 1
+#export var speed: float = 2
+export var distance: float = 5
 
 export var target_path: NodePath
 onready var target: Spatial = get_node_or_null(target_path)
@@ -12,14 +12,9 @@ func _physics_process(delta: float) -> void:
 	if not target:
 		return
 
-	var current_distance: float = global_transform.origin.distance_to(
-		target.global_transform.origin
-	)
-	var direction: Vector3 = global_transform.origin.direction_to(target.global_transform.origin)
-
+	var direction: Vector3 = global_transform.origin - target.global_transform.origin
+	var length: float = (global_transform.origin - target.global_transform.origin).length()
+	if length > distance:
+		global_transform.origin = target.global_transform.origin + direction * distance / length
+	
 	look_at(target.global_transform.origin, Vector3.UP)
-
-	if current_distance > distance:
-		global_transform.origin = global_transform.origin.linear_interpolate(
-			target.global_transform.origin + direction * distance, delta
-		)
