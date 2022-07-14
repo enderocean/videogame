3466bd34d63b066d3d417973af9e8c86d9821d85
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-07-14 18:17:04 +0200

    Started login screen

---
+++ scripts/ui/login_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _on_login_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_without_login_pressed() -> void:
+	pass # Replace with function body.
+
