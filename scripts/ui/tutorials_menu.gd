a6bcaf0e81c4f4e67f099db19168667dd0cff365
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-07-25 18:23:36 +0200

    Fixed sorted tutorials not loading the correct scene

--- scripts/ui/tutorials_menu.gd
+++ scripts/ui/tutorials_menu.gd
@@ -3,14 +3,15 @@ extends Control
 export var button_scene: PackedScene = preload("res://scenes/ui/tutorials/tutorial_button.tscn")
 
 export var buttons_list_path: NodePath
-var buttons_list: VBoxContainer
 
+var buttons_list: VBoxContainer
+var sorted_tutorials_id: Array
 
 func _ready() -> void:
 	buttons_list = get_node(buttons_list_path)
 	
 	# Sort tutorials by id
-	var sorted_tutorials_id: Array = Globals.tutorials.keys()
+	sorted_tutorials_id = Globals.tutorials.keys()
 	sorted_tutorials_id.sort()
 	
 	# Create tutorials button
@@ -25,7 +26,7 @@ func _ready() -> void:
 
 func _on_button_pressed(index: int) -> void:
 	SceneLoader.load_scene("res://scenes/ui/hud.tscn")
-	SceneLoader.load_scene(Globals.tutorials.values()[index].scene, true)
+	SceneLoader.load_scene(Globals.tutorials.get(sorted_tutorials_id[index]).scene, true)
 
 
 func _on_back_pressed() -> void:
