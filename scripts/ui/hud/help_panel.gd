7eedf2578b4d14abb8b0477d9ecb0dff72501ceb
denis.bled@newmips.com <denis.bled@newmips.com>
2022-07-27 16:27:56 +0200

    Help

--- scripts/ui/hud/help_panel.gd
+++ scripts/ui/hud/help_panel.gd
@@ -5,7 +5,6 @@ func _input(event):
 	if event.is_action_pressed("help"):
 		if not is_visible():
 			var help = get_child(0).get_node("help")
-			print(help.text)
 			help.text = tr(help.text)
 			show()
 		else:
